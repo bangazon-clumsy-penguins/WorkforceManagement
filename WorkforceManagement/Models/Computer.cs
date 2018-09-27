584ef4fcb364c275e9760098a806c3bad76330ae
awieckert <wiec1369@gmail.com>
2018-09-27 09:42:30 -0500

    Setting up the Computer index view

---
+++ WorkforceManagement/Models/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public DateTime DecommissionDate { get; set; }
+    }
+}
